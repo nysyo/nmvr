d4e8e90b8b6ecb89c729618ee405b9a0858038e2
nysyo <tds24004@gmail.com>
2019-08-20 22:15:13 +0900

    first commit

---
+++ NewSuperMISWVR/Assets/Controller.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Controller : MonoBehaviour
+{
+    private float StickToGroundForce;
+    //そのままの重力加速度だとふわふわなので自然な動きにするために重力加速度を強くする
+    private float MultipleGravity;
+    [SerializeField] private float speed;
+
+    private CharacterController CharacterController;
+    private Vector2 inputSpeed;
+    private Vector3 MoveDir;
+    private CollisionFlags CollisionFlags;
+    private Camera m_Camera;
+    private Mouse MouseLook;
+    // Use this for initialization
+    void Start()
+    {
+        StickToGroundForce = 10f;
+        MultipleGravity = 3f;
+        speed = 10f;
+
+        inputSpeed = new Vector2(0, 0);
+        CharacterController = GetComponent<CharacterController>();
+        m_Camera = Camera.main;
+        MouseLook = GetComponent<Mouse>();
+        MouseLook.Init(transform, m_Camera.transform);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MouseLook.LookRotation(transform, m_Camera.transform);
+    }
+
+    //一定時間ごとに実行
+    private void FixedUpdate()
+    {
+        GetInput();
+        Vector3 desiredMove = transform.forward * inputSpeed.y + transform.right * inputSpeed.x;
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, CharacterController.radius, Vector3.down, out hitInfo,
+                           CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+
+        MoveDir.x = desiredMove.x * speed;
+        MoveDir.z = desiredMove.z * speed;
+
+        if (CharacterController.isGrounded)
+        {
+            MoveDir.y = -StickToGroundForce;
+        }
+        else
+        {
+            MoveDir += Physics.gravity * MultipleGravity * Time.fixedDeltaTime;
+        }
+        CollisionFlags = CharacterController.Move(MoveDir * Time.fixedDeltaTime);
+
+        MouseLook.UpdateCursorLock();
+    }
+
+    private void GetInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        inputSpeed = new Vector2(horizontal, vertical);
+
+        //入力を標準ベクトルにする
+        if (inputSpeed.sqrMagnitude > 1)
+        {
+            inputSpeed.Normalize();
+        }
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (CollisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(CharacterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+}
---
+++ NewSuperMISWVR/Assets/Mouse.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+    public float smoothTime = 5f;
+    public bool lockCursor = true;
+
+
+    private Quaternion m_CharacterTargetRot;
+    private Quaternion m_CameraTargetRot;
+    private bool m_cursorIsLocked = true;
+
+    public void Init(Transform character, Transform camera)
+    {
+        m_CharacterTargetRot = character.localRotation;
+        m_CameraTargetRot = camera.localRotation;
+    }
+
+
+    public void LookRotation(Transform character, Transform camera)
+    {
+
+        float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+        m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+        //カメラターゲットを一定の値に制限する
+        if (clampVerticalRotation)
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+        character.localRotation = m_CharacterTargetRot;
+        camera.localRotation = m_CameraTargetRot;
+        
+
+        UpdateCursorLock();
+    }
+
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //ロックカーソルがオンになっていないと視点の回転をしない
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        //escキー押したらカーソルが表示される
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        //マウスクリックしたらまたカーソルを消す
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        //カーソルオンならカーソルロックして見えなくする
+        //オフならロックしないし見えるようにする
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        //標準化？
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        //アングルを決定
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        //角度を制限
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+        //角度から傾きを求める
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+}
